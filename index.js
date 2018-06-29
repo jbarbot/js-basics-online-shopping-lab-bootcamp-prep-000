10062180fa4d843bbda4114938eab531e6945d9f
Jacob Barbot <barbotjacob@gmail.com>
2018-06-29 14:41:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function removeFromCart(item) {
       if (getCart()[i].itemName === item) {itemToRemove = getCart()[i].itemName
       } return itemToRemove;
       function removeFromCart(itemToRemove) {
-        
+        var itemIndex = cart.indexOf
       }
     }
   }
